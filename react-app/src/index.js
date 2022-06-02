04dabab2224a4e13318e9ca879ed570771aae49c
Devian4748 <devian4748@gmail.com>
2022-06-02 23:51:59 +0900

    Add State with Hooks Sample

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,61 +1,21 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { useState } from 'react'; // Import useState
 const root = createRoot(document.querySelector('#root'));
 
-// Shopping Cart
+// Counter
 //
-// function ShoppingCart(props) {
-//   return <div>{props.count} items in your cart</div>;
+// function Counter() {
+//   const [seconds, setSeconds] = useState(0);
+//   return <h2>{seconds} seconds elapsed.</h2>;
 // }
-// root.render(<ShoppingCart count='5' />);
-
-// More props
 //
-// function Navbar(props) {
-//   return (
-//     <>
-//       <div>Welcome {`${props.user.first_name} ${props.user.last_name}`}</div>
-//       <p>You've got {props.notifications.length} notifications</p>
-//     </>
-//   );
-// }
+// root.render(<Counter />);
 
-// const notificatinos = [
-//   { id: 1, text: 'Order delivered' },
-//   { id: 2, text: 'Order received' },
-// ];
-
-// const user = {
-//   first_name: 'Sam',
-//   last_name: 'Wolf',
-// };
-// root.render(<Navbar notifications={notificatinos} user={user} />);
-
-// Button Ⅰ
+// Counterdown
 //
-// function Button(props) {
-//   return <button className={`btm-${props.size ?? 'medium'}`}></button>;
-// }
+function Counterdown() {
+  const [count, setCount] = useState(10);
+  return <h2>{count} times remaining</h2>;
+}
 
-// root.render(
-//   <>
-//     <Button size='small' />
-//     <Button size='large' />
-//     <Button />
-//   </>
-// );
-
-// Button Ⅱ
-//
-// function Button(props) {
-//   return <button>{props.children}</button>;
-// }
-// root.render(<Button>Login</Button>);
-
-// Destructuring props
-//
-// function Button({ className, children }) {
-//   return <button className={className}>{children}</button>;
-// }
-// root.render(<Button className='primary'>Login</Button>);
+root.render(<Counterdown />);
