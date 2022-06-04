35033b1028f210bad1a80e05bce5bab0fdd1fdb8
Devian4748 <devian4748@gmail.com>
2022-06-04 23:24:32 +0900

    Add conditional state change sample

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,63 +3,148 @@ import { useState } from 'react'; // Import useState
 
 const root = createRoot(document.querySelector('#root'));
 
-// Change event
+// Proper Countdown
 //
-// const Navbar = () => {
-//   return (
-//     <select
-//       className='countries'
-//       onChange={() => console.log('Country Changed')}
-//     >
-//       <option>Netherlands</option>
-//       <option>Belgium</option>
-//       <option>France</option>
-//     </select>
-//   );
-// };
-// root.render(<Navbar />);
+// function Countdown() {
+//   const [times, setTimes] = useState(5);
 
-// Name event handler
-//
-// function Counter() {
-//   const handleIncrementClick = () => {
-//     console.log('Button clicked');
+//   const handleDecrementClick = () => {
+//     if (times > 0) {
+//       setTimes(times - 1);
+//     }
 //   };
 
-//   return <button onClick={handleIncrementClick}>Add 1</button>;
-// }
-// root.render(<Counter />);
+//   const isDisabledBtn = times <= 0;
 
-// Counter
-//
-// function Counter() {
-//   const [times, setTimes] = useState(0);
-//   const handleIncrementClick = () => {
-//     setTimes(times + 1);
-//   };
 //   return (
 //     <>
-//       <h2>{times} times clicked</h2>
+//       <h2>{times} times remaining</h2>
+//       <button disabled={isDisabledBtn} onClick={handleDecrementClick}>
+//         Countdown
+//       </button>
+//     </>
+//   );
+// }
+// root.render(<Countdown />);
+
+// Resetting Clock
+//
+// function Clock() {
+//   const [seconds, setSeconds] = useState(0);
+
+//   const handleIncrementClick = () => {
+//     if (seconds < 59) {
+//       setSeconds(seconds + 1);
+//     } else {
+//       setSeconds(0);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h1>{seconds} seconds</h1>
 //       <button onClick={handleIncrementClick}>Add 1</button>
 //     </>
 //   );
 // }
-// root.render(<Counter />);
 
-// Countdown
+// root.render(<Clock />);
+
+// Counter with modes
 //
-function Countdown() {
-  const [count, setCount] = useState(10);
+// function App() {
+//   const [mode, setMode] = useState('Increment');
 
-  const handleDecrementClick = () => {
-    setCount(count - 1);
+//   return (
+//     <>
+//       <Mode mode={mode} onChangeMode={setMode} />
+//       <Counter mode={mode} />
+//     </>
+//   );
+// }
+
+// function Mode({ mode, onChangeMode }) {
+//   const handleModeChange = ({ target: { value } }) => {
+//     onChangeMode(value);
+//   };
+
+//   return (
+//     <select onChange={handleModeChange} value={mode}>
+//       <option>Increment</option>
+//       <option>Decrement</option>
+//     </select>
+//   );
+// }
+// function Counter({ mode }) {
+//   const [times, setTimes] = useState(0);
+
+//   const handleTimesClick = () => {
+//     if (mode === 'Increment') {
+//       setTimes(times + 1);
+//     } else if (mode === 'Decrement') {
+//       setTimes(times - 1);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h2>{mode}ing mode</h2>
+//       <h3>{times} times</h3>
+//       <button onClick={handleTimesClick}>{mode}</button>
+//     </>
+//   );
+// }
+// root.render(<App />);
+
+// Configurable Counter
+//
+function App() {
+  const [incrementBy, setIncrementBy] = useState(1);
+
+  return (
+    <>
+      <InputIncrementBy
+        incrementBy={incrementBy}
+        onChangeIncrementBy={setIncrementBy}
+      />
+      <Counter incrementBy={incrementBy} />
+    </>
+  );
+}
+
+const InputIncrementBy = ({ incrementBy, onChangeIncrementBy }) => {
+  const handleIncrementByChange = ({ target: { value } }) => {
+    onChangeIncrementBy(value);
   };
 
   return (
     <>
-      <h2>{count} times remaining</h2>
-      <button onClick={handleDecrementClick}>Count Down</button>
+      <label htmlFor='increment'>Increment By </label>
+      <input
+        id='increment'
+        type='number'
+        onChange={handleIncrementByChange}
+        value={incrementBy}
+      />
     </>
   );
-}
-root.render(<Countdown />);
+};
+
+const Counter = ({ incrementBy }) => {
+  const [times, setTimes] = useState(0);
+
+  const handleTimesClick = () => {
+    setTimes(
+      times + (incrementBy === '' ? 0 : Number.parseInt(incrementBy, 10))
+    );
+  };
+
+  return (
+    <>
+      <h2>{times} times</h2>
+      <button onClick={handleTimesClick}>Add {incrementBy}</button>
+    </>
+  );
+};
+
+root.render(<App />);
