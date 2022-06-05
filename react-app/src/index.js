ce21950d5d500cc9c1827be6871615b89e89885d
Devian4748 <devian4748@gmail.com>
2022-06-06 01:19:07 +0900

    Add State with Arrays Sample

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,54 +3,99 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
 
-// Countdown with lives
+// List Apps
 //
-// function Countdown() {
-//   const [count, setCount] = useState(5);
-//   const [live, setLive] = useState(3);
+// function Apps({ items }) {
+//   return (
+//     <ul>
+//       {items.map((item, index) => (
+//         <li key={index}>{item}</li>
+//       ))}
+//     </ul>
+//   );
+// }
 
-//   const handleDecrementClick = () => {
-//     if (count > 0) {
-//       setCount(count - 1);
-//     } else {
-//       setCount(5);
-//       setLive(live - 1);
-//     }
+// const apps = ['Calculator', 'Phone', 'Messages', 'Maps'];
+// root.render(<Apps items={apps} />);
+
+// List Products
+//
+// function Products() {
+//   const [products, setProducts] = useState([
+//     'Almond',
+//     'Cashew',
+//     'Rice',
+//     'Water',
+//   ]);
+//   return (
+//     <ul>
+//       {products.map((product, index) => (
+//         <li key={index}>{product}</li>
+//       ))}
+//     </ul>
+//   );
+// }
+// root.render(<Products />);
+
+// Shopping Wallet
+//
+// function Wallet() {
+//   const [transactions, setTransactions] = useState([]);
+
+//   const handleDepositClick = () => {
+//     setTransactions([...transactions, 10]);
+//   };
+
+//   const handleWithdrawClick = () => {
+//     setTransactions([...transactions, -10]);
+//   };
+
+//   const handleResetClick = () => {
+//     setTransactions([]);
 //   };
 
 //   return (
 //     <>
-//       <h2>Attempts remaining: {count}</h2>
-//       <button onClick={handleDecrementClick}>Count down</button>
-//       <h3>Lives remaining: {live}</h3>
+//       <button onClick={handleDepositClick}>Deposit 10</button>
+//       <button onClick={handleWithdrawClick}>Withdraw 10</button>
+//       <h2>
+//         Total: {transactions.reduce((total, current) => total + current, 0)}
+//       </h2>
+//       <button onClick={handleResetClick}>Reset</button>
+//       <ul>
+//         {transactions.map((transaction, index) => (
+//           <li key={index}>{transaction}</li>
+//         ))}
+//       </ul>
 //     </>
 //   );
 // }
+// root.render(<Wallet />);
 
-// root.render(<Countdown />);
-
-// Conditional counter
+// Undo action
 //
-// function Countdown({ enabled }) {
-//   const [count, setCount] = useState(5);
+function Wallet() {
+  const [transactions, setTransactions] = useState([]);
+  const handleDepositClick = () => {
+    setTransactions([...transactions, transactions.length + 1]);
+  };
 
-//   const handleDecrementClick = () => {
-//     if (enabled) {
-//       setCount(count - 1);
-//     }
-//   };
+  const handleUndoClick = () => {
+    // end not included
+    setTransactions(transactions.slice(0, transactions.length - 1));
+  };
 
-//   return (
-//     <>
-//       <h2>Attempts remaining: {count}</h2>
-//       <button onClick={handleDecrementClick}>Count down</button>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <button onClick={handleDepositClick}>Deposit</button>
+      <button onClick={handleUndoClick}>Undo</button>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <li key={index}>{transaction}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
-// root.render(
-//   <>
-//     <Countdown enabled={true} />
-//     <Countdown enabled={false} />
-//   </>
-// );
+root.render(<Wallet />);
