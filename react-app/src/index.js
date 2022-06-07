bef99420e6d3f0c87637f7b8af087033434e78a9
Devian4748 <devian4748@gmail.com>
2022-06-07 20:56:47 +0900

    Revert "Add Props Sample"
    This reverts commit c9819fc79cde74a6a19af7a57735c253de49dcad.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Button from './Button';
+import Hero from './Hero';
+import Link from './Link';
 
 const root = createRoot(document.querySelector('#root'));
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 //
 // function Navbar(props) {
@@ -74,3 +78,45 @@ function RegisterForm() {
 }
 root.render(<RegisterForm />);
 
+=======
+// Hero Component
+//
+// const Hero = () => {
+//   return (
+//     <>
+//       <h1>Welcome to our supermarket</h1>
+//       <p>Start shopping</p>
+//     </>
+//   );
+// };
+// root.render(<Hero />);
+
+// Navbar Component
+//
+// function Navbar() {
+//   return (
+//     <>
+//       <h3>Supermarket</h3>
+//       <p>Start shopping</p>
+//     </>
+//   );
+// }
+// root.render(React.createElement(Navbar, null));
+
+// One Component per file
+//
+// root.render(<Hero />);
+
+// Mutiple Components
+//
+function App() {
+  return (
+    <>
+      <Button />
+      <Button />
+      <Link />
+    </>
+  );
+}
+root.render(<App />);
+>>>>>>> parent of c9819fc (Add Props Sample)
