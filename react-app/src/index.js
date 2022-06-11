00a4ac4e7818ad8719ec13ec3d165a118ddd901e
Devian4748 <devian4748@gmail.com>
2022-06-12 02:17:38 +0900

    Revert "Add Component Sample"
    This reverts commit 83447d28d71365637b63d7aae891dbca16013959.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
 
+<<<<<<< HEAD
 // Clock Component
 //
 // const App = () => {
@@ -53,7 +54,60 @@ function App() {
       <h2>{counter}</h2>
       <button onClick={handleCounterToggle}>Start / Pause</button>
       <button onClick={handleCounterReset}>Stop</button>
+=======
+// Payment methods dropdown
+//
+// function getPaymentMethods() {
+//   return (
+//     <select className='payment-methods'>
+//       <option className='payment-method'>Card</option>
+//       <option className='payment-method'>Paypal</option>
+//       <option className='payment-method'>Cash on delivery</option>
+//     </select>
+//   );
+// }
+
+// root.render(getPaymentMethods());
+
+// Dropdown menu
+//
+// function getDropdownMenu() {
+//   return (
+//     <ul id='dropdown-menu' class='dropdown'>
+//       <li>Profile</li>
+//       <li>Orders</li>
+//       <li class='separator'></li>
+//       <li>Logout</li>
+//     </ul>
+//   );
+// }
+// root.render(getDropdownMenu());
+
+// User Avatar
+//
+// function getUserAvatar(path) {
+//   return <img src={path} alt='user avatar' />;
+// }
+// root.render(
+//   getUserAvatar(
+//     'https://res.cloudinary.com/dbfn5lnvx/image/upload/v1576923026/react-tutorial/misc/user.png'
+//   )
+// );
+
+// Footer
+//
+function getFooter() {
+  return (
+    <>
+      <h3>Your online supermarket</h3>
+      <ul>
+        <li>Contact</li>
+        <li>Press</li>
+        <li>About</li>
+      </ul>
+      <p>All rights reserved &copy;</p>
+>>>>>>> parent of 83447d2 (Add Component Sample)
     </>
   );
 }
-root.render(<App />);
+root.render(getFooter());
